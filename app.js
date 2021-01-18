8bec92102383e617c183f7624232663126cff17f
Gamertod <cavesagarcia@hotmail.com>
2021-01-18 01:24:29 -0400

    Modified Back End files so it disables the API
    Now the Back-End has no API routes, having them with no Front-End it's
    not a good idea. Now the Back-End connects to no database therefore the
    package mongoose it's not in the package.json file.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 const keys = require('./config/keys.js');
 const db = require('./models/mongoose');
 const bodyParser = require('body-parser');
@@ -13,24 +13,16 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'dist', 'mean-todos-list')));
 
 // Connect to database
+/*
 mongoose.connect(keys.mongooseURI, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connection.once('open', () => console.log('Connected to Database'))
 mongoose.connection.on('error', (err) => console.log('DB Error: ', err))
+*/
 
 // Body parser
 app.use(bodyParser.urlencoded({extended: false}))
 
-// Cors
-/*
-let corsOptions = {
-	origin: '*',
-	optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions));
-*/
-
-
-app.use('/api', require('./api-routes/api'));
+//app.use('/api', require('./api-routes/api'));
 
 app.use('/*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'dist/mean-todos-list/index.html'))
